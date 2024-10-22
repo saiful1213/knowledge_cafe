6a6e0a3db362867e5a0b74dcb558e39028e9cfa4
saiful islam <saifulislamptcbd@gmail.com>
2024-10-22 12:00:24 -0700

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="max-w-7xl mx-auto py-2">
+      <h1 className='text-4xl text-center font-bold bg-red-300'>Knowledge Cafe</h1>
+      <Button variant={'destructive'} size={'lg'}>ShadCn</Button>
+    </div>
+  )
+}
+
+export default App
